14e4c01d47c81424858b3d614a975776003c21c2
madisenbpatrick <madisen.bpatrick@gmail.com>
2025-10-22 16:35:23 -0400

    Created Gallery page that says "coming soon"

--- madisen-patrick-web/src/pages/GalleryPage/Gallery.tsx
+++ madisen-patrick-web/src/pages/GalleryPage/Gallery.tsx
@@ -1,10 +1,18 @@
+import React from "react";
 import Navbar from "../../components/common/Navbar";
+import { useFade } from "../../hooks/useFade";
+import "./Gallery.css";
+
 export default function Gallery() {
-    return (
-      <div>
-        <Navbar />
-        <h1>Welcome to Madisen Patrick's Website</h1>
-        <p>This is the Gallery page.</p>
+  const { fadeIn } = useFade(400);
+
+  return (
+    <>
+      <Navbar />
+      <div className={`gallery-container ${fadeIn ? "fade-in" : ""}`}>
+        <h1 className="gallery-title">Gallery</h1>
+        <p className="gallery-subtitle">Photos Coming Soon</p>
       </div>
-    );
-  }
+    </>
+  );
+}
