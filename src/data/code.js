c21a0773c79538e2e13ee7c42e600d503da4bd78
madisenbpatrick <madisen.bpatrick@gmail.com>
2025-10-30 12:32:20 -0400

    added new projects to list

--- src/data/code.js
+++ src/data/code.js
@@ -13,4 +13,16 @@ export default [
         tech: ["JavaScript","React Native", "Firebase", "Node.js"],
         github: "https://github.com/madisenbpatrick1/check-the-courts",
       },
+      {
+        name:"Madisen P Portfolio",
+        description: "A professional portfolio built with React and TypeScript featuring animated transitions and modular design.",
+        tech:["React","TypeScript","Tailwind CSS"],
+        github:"https://github.com/madisenbpatrick1/madisen-p-portfolio-website",
+    },
+    {
+        name:"Check It Off",
+        description: "Mobile app that helps users manage tasks and to-do lists in a minimalist design.",
+        tech:["XCode","SwiftUI","Product Management", "Figma"],
+        github:"",
+    },
 ];
